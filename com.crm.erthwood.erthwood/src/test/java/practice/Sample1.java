6932d2d00f25ce621b564c78af24f3c2dc3e0d3e
Praveen <Praveen@LAPTOP-RQMIKUIO>
2022-07-16 21:25:36 +0530

    added practice module

---
+++ com.crm.erthwood.erthwood/src/test/java/practice/Sample1.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class Sample1 {
+	
+	public void createContactTest() {
+		System.out.println("createContactTest executed");
+	}
+
+	public void createOrganizationTest() {
+		System.out.println("createOrganizationTest executed");
+	}
+}
