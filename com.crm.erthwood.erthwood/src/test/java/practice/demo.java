d85169c27c204f5ee9dfcd533c1bccdb4f7db6d7
vijay <vijay@LAPTOP-FK7QL8FC>
2022-07-18 12:46:09 +0530

    check that new update or not

---
+++ com.crm.erthwood.erthwood/src/test/java/practice/demo.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class demo {
+@Test
+public void newupdate()
+{
+	System.out.println("hello");
+}
+}
--- com.crm.erthwood.erthwood/src/test/java/practice/hello.java
+++ com.crm.erthwood.erthwood/src/test/java/practice/hello.java
@@ -4,11 +4,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hii");
-System.out.println("hello");
 
-
-System.out.println("**************");
+		System.out.println("hii hello");
 	}
 
 }
