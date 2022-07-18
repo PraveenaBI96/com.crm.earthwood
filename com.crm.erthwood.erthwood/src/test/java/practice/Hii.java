22f253582ebf3ce8419ad7588372ca340b29eabc
vijay <vijay@LAPTOP-FK7QL8FC>
2022-07-18 16:38:58 +0530

    check hii class new created

---
+++ com.crm.erthwood.erthwood/src/test/java/practice/Hii.java
@@ -0,0 +1,15 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Hii {
+@Test
+public void hii()
+{
+System.out.println("hii");
+System.out.println("heelo");
+System.out.println("by prasanna");
+}
+
+
+}
