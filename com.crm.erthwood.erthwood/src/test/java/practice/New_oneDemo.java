3c36c0a831ac1483914cb194bbd57abd5f8ba835
vijay <vijay@LAPTOP-FK7QL8FC>
2022-07-18 17:41:19 +0530

    new one demo class created by prasanna

---
+++ com.crm.erthwood.erthwood/src/test/java/practice/New_oneDemo.java
@@ -0,0 +1,12 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class New_oneDemo {
+
+@Test
+public void oneclass()
+{
+System.out.println("new one class created by prasanna");
+}
+}
